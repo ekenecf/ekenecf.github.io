3e99a588841a531630b46e7ae96079f1a3364644
ekenecf <ekens2u@gmail.com>
2022-03-11 16:33:29 +0100

    created form session

---
+++ JSfiles/Form.js
@@ -0,0 +1,18 @@
+submit.addEventListener('click', validate);
+function validate(e) {
+    e.preventDefault();
+
+const userName = document.getElementById("name");
+
+const mail = document.getElementById('mail');
+const msg = document.getElementById('msg');
+
+if(!userName.value){
+    userName.classList.toggle('view');
+}else if(!mail.value){
+    mail.classList.toggle('view');
+}else if(!msg.value){
+    msg.classList.toggle('view');
+}else{
+    window.location ="./newpage.html"
+}}
--- JSfiles/Modal.js
+++ JSfiles/Modal.js
@@ -9,6 +9,7 @@ const Modalcan = document.querySelector('.Modalcan');
 const viewBtn = document.querySelectorAll('.prj-view-btn');
 const cross = document.querySelector('.cross');
 
+
 const Arrayprojects = [
   {
     title: 'Tonic',
@@ -56,27 +57,91 @@ const Arrayprojects = [
   },
 ];
 
-function createModal() {
-  for (let i = 0; i < viewBtn.length; i += 1) {
-    const event = function event() {
-      Modalh.innerHTML = Arrayprojects[i].title;
-      Modalrole.innerHTML = Arrayprojects[i].role;
-      Modalyear.innerHTML = Arrayprojects[i].year;
-      Modalpara.innerHTML = Arrayprojects[i].description;
-      ModalImg.src = Arrayprojects[i].imageLink;
-      Modalcan.innerHTML = Arrayprojects[i].company;
-      ModalWorks.innerHTML = '';
-      for (let j = 0; j < Arrayprojects[i].tags.length; j += 1) {
-        ModalWorks.innerHTML = `${ModalWorks.innerHTML}<li class="tag">${Arrayprojects[i].tags[j]}</li>`;
-      }
-      Modalcontainer.classList.toggle('vanish');
-    };
-    viewBtn[i].addEventListener('click', (event));
+const renderCard = (card) => {
+  const cards = document.createElement('div');
+  cards.classList.add('Modalcont');
+
+
+  cards.innerHTML = `<div>
+  <div class="Modalhead">
+  <h2 class="Modalh"></h2>
+  <span class="cross">&times;</span>
+</div>
+<div class="Modallang">
+  <p class="Modalcan"></p>
+  <ul class="Modallist">
+      <li class="Modalrole"></li>
+      <li class="Modalyear"></li>
+  </ul>
+</div>
+<div class="Modalimage"><img src="#" class="ModalImg" alt=""></div>
+<div class="Modalwp">
+  <p class="Modalpara">
+  </p>
+  <div class="Modalw">
+      <ul class="ModalWorks">
+
+      </ul>
+      <ul class="ModalWorks2">
+          <li class="tag">Github</li>
+          <li class="tag">Ruby</li>
+          <li class="tag">Bootstrap</li>
+      </ul>
+
+      <div class="Modalbuttons">
+          <button class="Modalbtn1">See live <img src="./images/Vector.svg" alt=""></button>
+          <button class="Modalbtn1">See sources <img src="./images/Icon.svg" alt=""></button>
+      </div>
+  </div>
+</div>`;
+  return card;
+};
+
+const renderCards = () => {
+  const Projectdom = document.querySelector('.projectdom');
+  const modalCard = document.createElement('div');
+  modalCard.classList.add('Modalcontainer', 'vanish');
+
+  modalCard.appendChild(renderCard(card));
+  Projectdom.appendChild(modalCard);
+
+
+  function createModal() {
+    for (let i = 0; i < viewBtn.length; i += 1) {
+      const event = function event() {
+        // when this is triggered call  renderCards
+        Modalh.innerHTML = Arrayprojects[i].title;
+        Modalrole.innerHTML = Arrayprojects[i].role;
+        Modalyear.innerHTML = Arrayprojects[i].year;
+        Modalpara.innerHTML = Arrayprojects[i].description;
+        ModalImg.src = Arrayprojects[i].imageLink;
+        Modalcan.innerHTML = Arrayprojects[i].company;
+        ModalWorks.innerHTML = '';
+        for (let j = 0; j < Arrayprojects[i].tags.length; j += 1) {
+          ModalWorks.innerHTML = `${ModalWorks.innerHTML}<li class="tag">${Arrayprojects[i].tags[j]}</li>`;
+        }
+        Modalcontainer.classList.toggle('vanish');
+      };
+      viewBtn[i].addEventListener('click', (event));
+    }
   }
-}
-createModal();
+  createModal();
+};
+renderCards();
+
+
+
+
+
+
+
+
+
+
+
+
 
 function close() {
   Modalcontainer.classList.toggle('vanish');
 }
-cross.addEventListener('click', close);
+cross.addEventListener('click', close); 

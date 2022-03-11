e2fc0d6a2d036334d7f1827b9c01951fc5478b7f
ekenecf <ekens2u@gmail.com>
2022-03-11 17:31:43 +0100

    fixed linter error

--- JSfiles/Form.js
+++ JSfiles/Form.js
@@ -1,18 +1,20 @@
-submit.addEventListener('click', validate);
+
 function validate(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-const userName = document.getElementById("name");
+  const userName = document.getElementById('name');
+  const submit = document.getElementById('submit')
+  const mail = document.getElementById('mail');
+  const msg = document.getElementById('msg');
 
-const mail = document.getElementById('mail');
-const msg = document.getElementById('msg');
-
-if(!userName.value){
+submit.addEventListener('click', validate);
+if (!userName.value) {
     userName.classList.toggle('view');
-}else if(!mail.value){
-    mail.classList.toggle('view');
-}else if(!msg.value){
-    msg.classList.toggle('view');
-}else{
-    window.location ="./newpage.html"
-}}
+} else if(!mail.value) {
+  mail.classList.toggle('view');
+}else if(!msg.value ){
+  msg.classList.toggle('view');
+}else {
+  window.location ="./newpage.html"
+}
+}
